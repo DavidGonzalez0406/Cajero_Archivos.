352b0cc1afa34a948956ff0eeb39cf2e13e5d5cc
DavidGonzalez0406 <132014778+DavidGonzalez0406@users.noreply.github.com>
2023-11-15 20:49:22 -0600

    Update Administrador.java

--- Administrador.java
+++ Administrador.java
@@ -1,63 +1,31 @@
 class Administrador {
+    private static final String LOG_FILE = "logs.txt";
     private static final String BILLETES_FILE = "billetes.dat";
-    private static final String LOGS_FILE = "logs.txt";
 
-    public void realizarAccionAdministrador() {
+    public void menuAdministrador() {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("\nAcciones disponibles (Modo Administrador):");
-            System.out.println("1. Mostrar logs");
-            System.out.println("2. Mostrar cantidad de billetes");
-            System.out.println("3. Eliminar registros");
-            System.out.println("4. Restablecer cantidad de billetes");
-            System.out.println("5. Salir del modo administrador");
-            System.out.print("Seleccione una opción: ");
+            System.out.println("\nAcciones disponibles como Administrador:");
+            System.out.println("1. Mostrar listado de acciones en el log");
+            System.out.println("2. Mostrar cantidad de billetes restantes");
+            System.out.println("3. Salir");
 
+            System.out.print("Seleccione una opción: ");
             int opcion = scanner.nextInt();
-            scanner.nextLine(); // Limpiar el buffer de entrada
 
             switch (opcion) {
                 case 1:
-                    mostrarLogs();
+                    mostrarLog();
                     break;
                 case 2:
                     mostrarCantidadBilletes();
                     break;
                 case 3:
-                    eliminarRegistros();
+                    System.exit(0);
                     break;
-                case 4:
-                    restablecerBilletes();
-                    break;
-                case 5:
-                    System.out.println("Saliendo del modo administrador.");
-                    return;
                 default:
                     System.out.println("Opción no válida. Intente de nuevo.");
             }
         }
     }
-
-    private void mostrarLogs() {
-        // Lógica para mostrar logs (sin cambios)
-    }
-
-    private void mostrarCantidadBilletes() {
-        // Lógica para mostrar cantidad de billetes (sin cambios)
-    }
-
-    private void eliminarRegistros() {
-        // Lógica para eliminar registros (puedes implementar según tus necesidades)
-    }
-
-    private void restablecerBilletes() {
-        // Lógica para restablecer cantidad de billetes (puedes implementar según tus necesidades)
-    }
-}
-class CajeroAutomático {
-    private Map<String, Integer> billetes;
-    private String billetesFile;
-
-    // Resto del código de la clase CajeroAutomático (sin cambios)
-}
