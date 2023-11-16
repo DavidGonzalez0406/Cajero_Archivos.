31717664b963b5e1b60c0e6d8089043334d7f396
DavidGonzalez0406 <132014778+DavidGonzalez0406@users.noreply.github.com>
2023-11-15 20:49:02 -0600

    Update CajeroAutomatico.java

--- CajeroAutomatico.java
+++ CajeroAutomatico.java
@@ -1,52 +1,89 @@
-import java.util.Scanner;
 
-public class CajeroAutomáticoMain {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Ingrese su nombre: ");
-        String nombre = scanner.nextLine();
-        System.out.print("Ingrese su NIP de 4 dígitos: ");
-        String nip = scanner.nextLine();
+class CajeroAutomático {
+    private Map<String, Integer> billetes;
+    private String billetesFile;
 
-        if (nombre.equals("admin") && nip.equals("3243")) {
-            // Acceder al modo administrador
-            Administrador administrador = new Administrador();
-            administrador.menuAdministrador();
-        } else {
-            // Acceder al modo cajero
-            Usuario usuario = new Usuario(nombre, nip);
-            CajeroAutomático cajero = new CajeroAutomático("billetes.dat");
+    public CajeroAutomático(String billetesFile) {
+        this.billetesFile = billetesFile;
+        cargarBilletes();
+    }
 
-            System.out.println("Bienvenido, " + usuario.getNombre() + " al modo cajero.");
-
-            while (true) {
-                System.out.println("\nAcciones disponibles:");
-                System.out.println("1. Consultar saldo");
-                System.out.println("2. Retirar efectivo");
-                System.out.println("3. Salir");
-
-                System.out.print("Seleccione una opción: ");
-                int opcion = scanner.nextInt();
-
-                switch (opcion) {
-                    case 1:
-                        cajero.consultarSaldo(usuario);
-                        Logger.log("consultar", usuario, usuario.getSaldo(), true);
-                        break;
-                    case 2:
-                        System.out.print("Ingrese la cantidad a retirar: ");
-                        int cantidad = scanner.nextInt();
-                        boolean retiroExitoso = cajero.retirarEfectivo(usuario, cantidad);
-                        Logger.log("retirar", usuario, retiroExitoso ? usuario.getSaldo() : 0, retiroExitoso);
-                        break;
-                    case 3:
-                        System.exit(0);
-                        break;
-                    default:
-                        System.out.println("Opción no válida. Intente de nuevo.");
-                }
-            }
+    private void cargarBilletes() {
+        try {
+            FileInputStream fileInputStream = new FileInputStream(billetesFile);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            billetes = (HashMap<String, Integer>) objectInputStream.readObject();
+            objectInputStream.close();
+        } catch (IOException | ClassNotFoundException e) {
+            billetes = new HashMap<>();
+            inicializarBilletes();
         }
     }
+
+    private void guardarBilletes() {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(billetesFile);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(billetes);
+            objectOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void inicializarBilletes() {
+        billetes.put("100", 100);
+        billetes.put("200", 100);
+        billetes.put("500", 20);
+        billetes.put("1000", 10);
+        guardarBilletes();
+    }
+    public void consultarSaldo(Usuario usuario) {
+        System.out.println("Saldo actual de " + usuario.getNombre() + ": $" + usuario.getSaldo());
+    }
+
+    public boolean retirarEfectivo(Usuario usuario, int cantidad) {
+int montoMinimo = Math.min(usuario.getSaldo(), calcularMontoMinimoBilletes());
+        System.out.println("Monto mínimo a retirar: $" + montoMinimo);
+
+        if (cantidad > usuario.getSaldo() || cantidad > montoMinimo) {
+            System.out.println("Fondos insuficientes o cantidad incorrecta. Operación cancelada.");
+            return false;
+        }
+
+        if (!verificarBilletesDisponibles(cantidad)) {
+            System.out.println("No hay billetes disponibles para entregar esa cantidad. Operación cancelada.");
+            return false;
+        }
+
+        actualizarBilletes(cantidad);
+        usuario = new Usuario(usuario.getNombre(), usuario.getNip()); // Actualizar saldo del usuario
+        usuario = new Usuario(usuario.getNombre(), usuario.getNip()); // Actualizar saldo del usuario
+
+        guardarBilletes();
+        System.out.println("Retiro exitoso. Nuevo saldo: $" + usuario.getSaldo());
+        return true;
+    }
+
+    private int calcularMontoMinimoBilletes() {
+        return billetes.get("100") * 100 + billetes.get("200") * 200 + billetes.get("500") * 500 + billetes.get("1000") * 1000;
+    }
+
+    private boolean verificarBilletesDisponibles(int cantidad) {
+        return cantidad <= calcularMontoMinimoBilletes();
+    }
+
+    private void actualizarBilletes(int cantidad) {
+
+        billetes.put("100", billetes.get("100") - cantidadBilletes("100", cantidad));
+        billetes.put("200", billetes.get("200") - cantidadBilletes("200", cantidad));
+        billetes.put("500", billetes.get("500") - cantidadBilletes("500", cantidad));
+        billetes.put("1000", billetes.get("1000") - cantidadBilletes("1000", cantidad));
+    }
+
+    private int cantidadBilletes(String denominacion, int cantidad) {
+        return cantidad / Integer.parseInt(denominacion);
+    }
 }
+    
